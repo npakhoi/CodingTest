917265cf86e0058953f1327a3933baccfbbca5a2
npakhoi <npakhoi@tma.com.vn>
2024-11-15 15:04:54 +0700

    implement FindFirstDuplicatedCharacter

--- ClassTechTest/StringTools.cs
+++ ClassTechTest/StringTools.cs
@@ -1,4 +1,6 @@
-﻿namespace ClassTechTest
+﻿using System.Collections.Generic;
+
+namespace ClassTechTest
 {
     public class StringTools
     {
@@ -6,6 +8,26 @@
 
         public string FindFirstDuplicatedCharacter(string input)
         {
+            if (string.IsNullOrEmpty(input))
+            {
+                return null;
+            }
+
+            var charDict = new Dictionary<char, char>();
+
+            foreach (var inputChar in input)
+            {
+                if (char.IsLetterOrDigit(inputChar))
+                {
+                    // Return its storaged value or add a new key-value pair.
+                    if (charDict.ContainsKey(char.ToUpper(inputChar)))
+                    {
+                        return charDict[char.ToUpper(inputChar)].ToString();
+                    }
+                    charDict[char.ToUpper(inputChar)] = inputChar;
+                }
+            }
+
             return null;
         }
 
